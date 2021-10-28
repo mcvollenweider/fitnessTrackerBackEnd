c7e7e2fa02eff326c188f6a1bc5630efcbf446da
HannahWroblewski <hannah.wrob@gmail.com>
2021-10-27 20:14:02 -0400

    pulled from mark

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // create the express server here
+const PORT = 3000;
+const express = require("express");
+const server = express();
+const morgan = require("morgan");
+server.use(morgan("dev"));
+server.use(express.json());
+const { client } = require("./db");
+client.connect();
+server.listen(PORT, () => {
+  console.log("The server is up on port", PORT);
+});
