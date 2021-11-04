a2c2602a58d0e9496668adb06092746aa0805f81
HannahWroblewski <hannah.wrob@gmail.com>
2021-11-03 20:18:38 -0400

    working on routines and routine_activities

--- db/client.js
+++ db/client.js
@@ -1,8 +1,8 @@
 // build and export your unconnected client here
 const { Client } = require("pg");
 const CONNECTION_STRING =
-  //process.env.DATABASE_URL || "postgres://postgres@localhost:5433/fitness-dev"; 
-   process.env.DATABASE_URL || "postgres://localhost:5432/fitness-dev"; 
+  process.env.DATABASE_URL || "postgres://postgres@localhost:5433/fitness-dev"; 
+  //  process.env.DATABASE_URL || "postgres://localhost:5432/fitness-dev"; 
 const client = new Client({
   connectionString: CONNECTION_STRING,
     ssl: process.env.NODE_ENV === "production"? {
