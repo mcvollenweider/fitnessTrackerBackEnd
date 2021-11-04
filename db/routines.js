a4d44c5c53d31daf1eafb996bc0e4d18db29e2b7
mcvollenweider <mcvollenweider@gmail.com>
2021-11-03 20:20:53 -0400

    pulling

--- db/routines.js
+++ db/routines.js
@@ -1,5 +1,5 @@
 const client = require("./client");
-const {attachActivitiesToRoutines} = require("./activities")
+const { attachActivitiesToRoutines } = require("./activities")
 
 async function getRoutineById(id) {
   try {
